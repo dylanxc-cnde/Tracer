eba1db915cb0b8535df146986e2cf35d79116d56
cexu_dylan <dylanalexxc@gmail.com>
2026-08-22 23:39:01 +0200

    feat: add dialog window for one card

--- frontend/src/components/PostingCardDetails.tsx
+++ frontend/src/components/PostingCardDetails.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react'
+
 import type { PostingCard } from '../postings/types/postingCard';
 
 type PostingCardDetailsProps = {
@@ -8,26 +10,39 @@ type PostingCardDetailsProps = {
 export function PostingCardDetails(
     {card, onClose,} : PostingCardDetailsProps
 ) {
-    return (
-        <article className="posting-card-details">
-            <h2 className="posting-card-details__header">
-                {card.posting_alias ??
-                    card.posting.identity.position_title?.value ??
-                    'Unknown Position'}
-            </h2>
+  const dialogRef = useRef<HTMLDialogElement>(null)
+  useEffect(() => {
+    const dialog = dialogRef.current
 
-            <p>
-                {card.posting.identity.company_name?.value ??
-                    'Unknown Company'}
-            </p>
+    if (dialog !== null && !dialog.open) {
+      dialog.showModal()
+    }
+  }, [])
 
-            <button
-             className="posting-card-details__close"
-             type="button"
-             onClick={onClose}
-            >
-                Close
-            </button>
-        </article>
-    )
+  return (
+    <dialog
+      ref={dialogRef}
+      className="posting-card-details"
+      onClose={onClose}
+    >
+      <h2 className="posting-card-details__header">
+        {card.posting_alias ??
+          card.posting.identity.position_title?.value ??
+          'Unknown Position'}
+      </h2>
+
+      <p>
+        {card.posting.identity.company_name?.value ??
+          'Unknown Company'}
+      </p>
+
+      <button
+        className="posting-card-details__close"
+        type="button"
+        onClick={() => dialogRef.current?.close()}
+      >
+        Close
+      </button>
+    </dialog>
+  )
 }
